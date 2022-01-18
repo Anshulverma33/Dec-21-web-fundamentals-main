1b26ce1d1921514d527285895a17a5f9bc2c16e8
Anshul verma <43145798+Anshulverma33@users.noreply.github.com>
2022-01-18 20:07:02 +0530

    commit

---
+++ Dec21-Web-Fundamentals-main/Jan-12/dom.js
@@ -0,0 +1,12 @@
+console.log('😂');
+var newTextaaaa = "This is new textkyfjarfafrewftyufewtya";
+//Alert something on click of the button.
+function alertSomething(arg1) {
+    // alert(arg1);
+    var aaaa = document.getElementById('abcd');
+    // console.log(something);
+    // aaaa.innerText = newTextaaaa;
+    aaaa.innerText = "<button>Another one!</button>";
+}
+
+//Get an element from the HTML document.
